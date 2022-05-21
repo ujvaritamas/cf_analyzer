5f6a4f4b5d3581c04149c04ebbab8d795a226e5b
ujvaritamas <49878381+ujvaritamas@users.noreply.github.com>
2022-05-21 16:29:57 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cf_analyzer
 cf_analyzer
+
+commands:
+python3 -m venv virtualenv
+source ./virtualenv/bin/activate  (win: .\virtualenv\Scripts\activate.bat)
+virtualenv\Scripts\python -m pip install --upgrade pip
+python -m pip install beautifulsoup4
+deactivate
---
+++ proj/test.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+url = "https://games.crossfit.com/leaderboard/open/2022?view=0&division=2&region=0&scaled=0&sort=0"
+page = urlopen(url)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+#print(page)
+
+soup = BeautifulSoup(html, "html.parser")
+
+#import pdb; pdb.set_trace()
+#table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('id') and tag['id']=="Table1") 
+#rows = table.findAll(lambda tag: tag.name=='tr'
+
+
+import pdb; pdb.set_trace()
+for a in soup.find_all("div", {"id": "leaderboardSponsorVisible"}):
+    for b in a.find_all("div", {"class": "inner desktop"}):
+        print(b)
+
+for a in soup.find_all('tbody'):
+    
+    print(a)
+    #print(data)
+print("haha")
